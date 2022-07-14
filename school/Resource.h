f3071be3dcc58feb8146d069270ad1cba42eeb0f
m200is <changhy27@naver.com>
2022-07-14 10:31:15 +0900

    2차 완성본

--- school/Resource.h
+++ school/Resource.h
@@ -1,9 +1,10 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++에서 생성한 포함 파일입니다.
-// school.rc에서 사용되고 있습니다.
+// Microsoft Visual C++ generated include file.
+// Used by school.rc
 //
 #define IDC_MYICON                      2
 #define IDD_SCHOOL_DIALOG               102
+#define IDC_SCHOOL_DIALOG               102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -12,6 +13,8 @@
 #define IDI_SMALL                       108
 #define IDC_SCHOOL                      109
 #define IDR_MAINFRAME                   128
+#define IDC_LIST1                       1000
+#define IDC_LIST2                       1001
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -19,9 +22,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- school/school.cpp
+++ school/school.cpp
@@ -1,11 +1,17 @@
 ﻿// school.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+//#pragma warning(disable:4996)
 #include "framework.h"
 #include "school.h"
+#include "resource.h"
 #include <ctime>
-#include<fstream>
+#include <fstream>
 #include <string.h>
+#include <ctime>
+#include <array>
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,24 +23,49 @@ struct tm* t;
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-wchar_t guest_name[100][100];                      //방문객 이름
-wchar_t out_guest[100];
-int guest_count = 0;                            //방문객 인원수
+wchar_t guest_name_in[1000][100];               //방문객 이름
+wchar_t guest_name_out[1000][100];				//나간 방문객
+wchar_t out_guest[100];							//임시 방문객변수
+wchar_t in_guest[100];							
+wstring guest_intime[100];						//시간 변수
+wstring guest_outtime[100];
+int guest_count_in = 0;                            //방문객 인원수
+int guest_count_out = 0;
+int gcountin;					//for문용
+int gcountout;					
 int check_inlist = 0;
+wchar_t* widecstr;				//wstring to wchar_t용
+size_t tmp = 0;
+size_t csize;
+int flagin = 0;
+int flagout = 0;
+
+std::wstring currentDateTime()
+{
+	time_t rawtime;
+	struct tm timeinfo;
+	wchar_t buffer[20];
+	time(&rawtime);
+	localtime_s(&timeinfo, &rawtime);
+	wcsftime(buffer, 20, L"%Y-%m-%d %H:%M:%S", &timeinfo);
+	return buffer;
+}
+
+std::wstring concatenate(wchar_t* list, wstring time) {
+	wchar_t guest_name[1000];
+	wcsncpy_s(guest_name, list, sizeof(list)+1);
+	wcscat_s(guest_name, 10, L"------");
+	const wchar_t* wcs = time.c_str();
+	wcscat_s(guest_name, 100, wcs);
+	return guest_name;
+}
+
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-// LPCWSTR to char
-void WCharToChar(const wchar_t* pwstrSrc, char pstrDest[])
-{
-	int nLen = (int)wcslen(pwstrSrc);
-	size_t ConvertedChars;
-	wcstombs_s(&ConvertedChars, pstrDest, sizeof(pstrDest), pwstrSrc, nLen + 1);
-}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -131,6 +162,40 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 }
 
 
+BOOL CALLBACK DialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	HWND hList1 = GetDlgItem(hDlg, IDC_LIST1);
+	HWND hList2 = GetDlgItem(hDlg, IDC_LIST2);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		for (int i = 0; i < guest_count_in; i++) {
+			widecstr = _wcsdup(concatenate(guest_name_in[i], guest_intime[i]).c_str());	//입장 기록
+			SendMessage(hList1, LB_ADDSTRING, 0, (WPARAM)widecstr);
+			if (wcsncmp(guest_name_out[i], L"", sizeof(guest_name_out[i]))!=0) {
+				widecstr = _wcsdup(concatenate(guest_name_out[i], guest_outtime[i]).c_str());
+				SendMessage(hList2, LB_ADDSTRING, 0, (WPARAM)widecstr);
+			}
+		}
+		return TRUE;
+	case WM_COMMAND:
+		
+		switch (wParam)
+		{
+		case IDCANCEL:
+			EndDialog(hDlg, 0);
+			SendMessage(hList1, LB_RESETCONTENT, 0, 0);	//리스트박스 초기화
+			SendMessage(hList1, LB_RESETCONTENT, 0, 0);
+			hList1 = NULL;
+			hList2 = NULL;
+			break;
+		}
+		return TRUE;
+	}
+	return FALSE;
+}
+
+
 //
 //  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -152,6 +217,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		hEdit = CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT, 375, 200, 250, 30, hWnd, (HMENU)1, hInst, NULL);		//입력 창 생성
 		CreateWindow(L"button", L"들어가기", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 375, 250, 120, 30, hWnd, (HMENU)2, hInst, NULL);			//버튼 생성
 		CreateWindow(L"button", L"나가기", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 505, 250, 120, 30, hWnd, (HMENU)3, hInst, NULL);
+		CreateWindow(L"button", L"출입기록 확인", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 10, 120, 30, hWnd, (HMENU)4, hInst, NULL);
 		break;
 	}
 	case WM_COMMAND:
@@ -159,30 +225,65 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (LOWORD(wParam))
 		{
 		case 2:																	//들어가기 버튼 눌렀을 때
-			GetWindowText(hEdit, guest_name[guest_count], 100);					//이름 배열에 추가
-			guest_count++;
-			MessageBox(hWnd, L"입장 처리되었습니다.", L"확인", MB_OK);
-			SetWindowText(hEdit, L"");
+			GetWindowText(hEdit, in_guest, 100);					//이름 배열에 추가
+			if (_tcscmp(L"", in_guest) == 0) {
+				MessageBox(hWnd, L"이름을 입력해주세요.", L"경고", MB_OK);
+				break;
+			}
+			flagin = 0;
+			flagout = 0;
+			if (guest_count_out == 0)
+				gcountout = guest_count_out + 1;
+			else
+				gcountout = guest_count_out;
+			if (guest_count_in == 0)
+				gcountin = guest_count_in + 1;
+			else
+				gcountin = guest_count_in;
+			for (int i = 0; i < gcountin; i++) {								//배열에서 이름 찾기
+				for (int j = 0; j < gcountout; j++) {
+					if ((_tcscmp(guest_name_in[i], in_guest) == 0)) {	//이름 있을 경우 퇴장 처리
+						flagin++;
+					}
+					if ((_tcscmp(guest_name_out[i], in_guest) == 0)) {	//이름 있을 경우 퇴장 처리
+						flagout++;
+					}
+				}
+			}
+			if (flagin == flagout) {
+				wcsncpy_s(guest_name_in[guest_count_in], in_guest, sizeof(in_guest) + 1);
+				guest_intime[guest_count_in] = currentDateTime();
+				guest_count_in++;
+				MessageBox(hWnd, L"입장 처리되었습니다.", L"확인", MB_OK);
+				SetWindowText(hEdit, L"");
+			}
+			else {
+				MessageBox(hWnd, L"이미 입장하셨습니다.", L"경고", MB_OK);
+			}
 			break;
 		case 3:
 			check = MessageBox(hWnd, L"퇴장하시겠습니까?", L"확인", MB_YESNO);		//퇴장 버튼 눌렀을 떄
-			GetWindowText(hEdit, out_guest, 100);
 			if (check == IDYES)
 			{
-				for (int i = 0; i < guest_count; i++) {								//배열에서 이름 찾기
-					if (_tcscmp(guest_name[i], out_guest) == 0) {					//이름 있을 경우 퇴장 처리
-						wcscpy_s(guest_name[i], 100, L"0x\9");
+				GetWindowText(hEdit, out_guest, 100);
+				for (int i = 0; i < guest_count_in; i++) {								//배열에서 이름 찾기
+					if (_tcscmp(guest_name_in[i], out_guest) == 0) {					//이름 있을 경우 퇴장 처리
+						wcsncpy_s(guest_name_out[guest_count_out], out_guest, sizeof(out_guest)+1);
+						guest_outtime[guest_count_out] = currentDateTime();
 						MessageBox(hWnd, L"퇴장 처리되었습니다.", L"확인", MB_OK);
 						check_inlist = 1;
 						SetWindowText(hEdit, L"");
+						guest_count_out++;
 						break;
 					}
 				}
 				if (check_inlist != 1) {			//이름 없으면 경고
-					MessageBox(hWnd, L"이름이 등록되어 있지 않습니다.", L"확인", MB_OK);
+					MessageBox(hWnd, L"이름이 등록되어 있지 않습니다.", L"경고", MB_OK);
 				}
 			}
 			break;
+		case 4:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_SCHOOL_DIALOG), hWnd,(DLGPROC)DialogProc);
 		}
 		break;
 	case WM_GETMINMAXINFO:
